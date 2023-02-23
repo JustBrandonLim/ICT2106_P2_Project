79cbb8bb8b9aa2dd7b0d03f0c38931fbb4b3742c
Norman Chia <norman.chia.minjie@gmail.com>
2023-02-23 22:33:36 +0800

    added backend calls for Device Store feature

---
+++ SmartHomeManager/SmartHomeManager.API/Controllers/DeviceStoreAPI/DeviceProductsController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartHomeManager.DataSource;
+using SmartHomeManager.Domain.DeviceStoreDomain.Entities;
+using SmartHomeManager.Domain.DeviceStoreDomain.Services;
+using SmartHomeManager.Domain.DeviceStoreDomain.Interfaces;
+
+
+namespace SmartHomeManager.API.Controllers.DeviceStoreAPI;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DeviceProductsController : ControllerBase
+{
+    private readonly  DeviceProductService _deviceProductService;
+
+    public DeviceProductsController(IDeviceProductsRepository deviceProductsRepository)
+    {
+        _deviceProductService = new DeviceProductService(deviceProductsRepository);
+    }
+
+    // GET: api/DeviceProducts
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<DeviceProduct>>> GetDeviceProducts()
+    {
+        return Ok(await _deviceProductService.GetAllDeviceProducts());
+    }
+
+    // POST: api/purchaseDevice
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<DeviceProduct>> PurchaseDeviceProduct(int deviceId, int quantity)
+    {
+        await _deviceProductService.PurchaseDevice(deviceId,quantity);
+
+        return Ok("Goodjob");
+    }
+    
+}
---
+++ SmartHomeManager/SmartHomeManager.DataSource/DeviceStoreDataSource/DeviceProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SmartHomeManager.Domain.DeviceStoreDomain.Entities;
+using SmartHomeManager.Domain.DeviceStoreDomain.Interfaces;
+using SmartHomeManager.Domain.RoomDomain.Entities;
+
+namespace SmartHomeManager.DataSource.DeviceStoreDataSource;
+
+public class DeviceProductRepository : IDeviceProductsRepository
+{
+
+    private readonly ApplicationDbContext _db;
+    private readonly DbSet<DeviceProduct> _dbSet;
+
+    public DeviceProductRepository(ApplicationDbContext db)
+    {
+        _db = db;
+        _dbSet = _db.Set<DeviceProduct>();
+    }
+
+
+    public async Task<DeviceProduct?> Get(int id)
+    {
+        var result = await _dbSet.FindAsync(id);
+
+        return result;
+    }
+
+    public async Task<IEnumerable<DeviceProduct>> GetAllDeviceProducts()
+    {
+        IEnumerable<DeviceProduct> query = await _dbSet.ToListAsync();
+        return query;
+    }
+
+    public void UpdateQuantity(DeviceProduct device)
+    {
+
+        _dbSet.Update(device);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _db.SaveChangesAsync();
+    }
+
+}
+
