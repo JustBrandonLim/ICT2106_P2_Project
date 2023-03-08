d7dce18bf7ba687e4f1649dcd6a6df3c08659385
brandonut <heelabc@gmail.com>
2023-03-09 01:55:37 +0800

    add settings page

--- SmartHomeManager/SmartHomeManager.View/src/components/Devices/ManageDeviceSettingsCard.js
+++ SmartHomeManager/SmartHomeManager.View/src/components/Devices/ManageDeviceSettingsCard.js
@@ -1,30 +1,24 @@
 import React, { useState } from "react";
-import { Heading, Card, CardHeader, CardBody, Text, CardFooter, Button, Select } from "@chakra-ui/react";
+import { Heading, Card, CardHeader, CardBody, Text, CardFooter, Button, Input } from "@chakra-ui/react";
 
 export default function ManageDeviceSettingsCard(props) {
     const [deviceSettings, setDeviceSettings] = useState(props.deviceSettings);
 
+    console.log(props)
+
     return (
         <Card>
             <CardHeader>
-                <Heading size="md">{`Set ${props.deviceSettings}`}</Heading>
+                <Heading size="xl">{`Modify ${props.deviceName} Settings`}</Heading>
             </CardHeader>
 
             <CardBody>
-                <Select value={deviceSettings ? deviceSettings.configurationValue : 0}>
-                    {deviceSettings.length > 0 ? (
-                        deviceSettings.map((settings, i) => (
-                            <option
-                                key={i}
-                                value={settings}
-                            >
-                                test
-                            </option>
-                        ))
-                    ) : (
-                        <p>None available.</p>
-                    )}
-                </Select>
+                <Input mt={5}>{props.deviceSerialNumber}</Input>
+                <Input mt={5}>{props.deviceBrand}</Input>
+                <Input mt={5}>{props.deviceModel}</Input>
+                <Input mt={5}>{props.deviceType}</Input>
+                <Input mt={5}>{props.deviceName}</Input>
+                <Input mt={5}>{props.devicePassword}</Input>
             </CardBody>
             <CardFooter flex="row" justifyContent="flex-end">
                 <Button colorScheme="green">Apply Changes</Button>
--- SmartHomeManager/SmartHomeManager.View/src/pages/device/manage-device/ManageDeviceSettings.js
+++ SmartHomeManager/SmartHomeManager.View/src/pages/device/manage-device/ManageDeviceSettings.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Text, Tabs, TabList, Tab, TabPanels, TabPanel, Stack } from "@chakra-ui/react";
+import { Container, Text, Tabs, TabList, Tab, TabPanels, TabPanel, Stack, Heading, Card, CardHeader, CardBody, CardFooter, Button, Input } from "@chakra-ui/react";
 import { useSearchParams } from "react-router-dom";
 import ManageDeviceSettingsCard from "../../../components/Devices/ManageDeviceSettingsCard";
 
@@ -10,8 +10,6 @@ export default function ManageDeviceSettings() {
 
   const [accountId, setAccountId] = useState("11111111-1111-1111-1111-111111111111");
 
-  console.log(deviceSettings);
-
   useEffect(() => {
     fetch(`https://localhost:7140/api/ManageDevice/GetDeviceById/${deviceId}`)
       .then((response) => response.json())
@@ -20,17 +18,38 @@ export default function ManageDeviceSettings() {
       });
   }, []);
 
+    const { deviceName, deviceBrand, deviceModel, deviceSerialNumber, deviceType, devicePassword  } = deviceSettings;
+
   return (
     <Container mt={5} mb={5} p={5} maxW="3xl" minH="50vh" border="1px" borderColor="gray.100" rounded="lg" boxShadow="lg" centerContent>
       <Text fontWeight="bold" fontSize="xl" mb={5}>
-        {`Manage ${deviceSettings} settings`}
+              {`Manage ${deviceName} settings`}
       </Text>
       <Stack spacing={5}>
-        {deviceSettings.length > 0 ? (
-          deviceSettings.map((configuration, i) => <ManageDeviceSettingsCard key={i} deviceSettings={deviceSettings} />)
-        ) : (
-          <p>None available.</p>
-        )}
+              <Card>
+                  <CardBody>
+                      <Heading size="md">Device Name</Heading>
+                      <Input defaultValue={deviceName} />
+
+                      <Heading mt={5} size="md">Device Password</Heading>
+                      <Input defaultValue={devicePassword} />
+
+                      <Heading mt={5} size="md">Device Serial Number</Heading>
+                      <Input defaultValue={deviceSerialNumber} />
+
+                      <Heading mt={5} size="md">Device Brand</Heading>
+                      <Input defaultValue={deviceBrand} />
+
+                      <Heading mt={5} size="md">Device Serial Model</Heading>
+                      <Input defaultValue={deviceModel} />
+
+                      <Heading mt={5} size="md">Device Serial Type</Heading>
+                      <Input defaultValue={deviceType} />
+                      </CardBody>
+                  <CardFooter flex="row" justifyContent="flex-end">
+                      <Button colorScheme="green">Apply Changes</Button>
+                  </CardFooter>
+              </Card>
       </Stack>
     </Container>
   );
