498e4a0b55556af638bc6f1551e76291cc7db24c
wangqixian15 <wangqixian18@gmail.com>
2023-03-14 18:42:45 +0800

    Manage device in 2D Home implemented

---
+++ SmartHomeManager/SmartHomeManager.Domain/TwoDHomeDomain/Services/TwoDHomeReadService.cs
@@ -0,0 +1,76 @@
+using SmartHomeManager.Domain.RoomDomain.Interfaces;
+using SmartHomeManager.Domain.RoomDomain.Mocks;
+using SmartHomeManager.Domain.TwoDHomeDomain.DTOs.Responses;
+using SmartHomeManager.Domain.TwoDHomeDomain.Entities;
+using SmartHomeManager.Domain.TwoDHomeDomain.Factories;
+using SmartHomeManager.Domain.TwoDHomeDomain.Interfaces;
+
+namespace SmartHomeManager.Domain.TwoDHomeDomain.Services;
+
+public class TwoDHomeReadService : ITwoDHomeReadService
+{
+    private readonly IDeviceInformationServiceMock _deviceInformationService;
+    private readonly IRoomRepository _roomRepository;
+    private readonly ITwoDHomeRepository _twoDHomeRepository;
+
+    public TwoDHomeReadService(
+        ITwoDHomeRepository twoDHomeRepository,
+        IRoomRepository roomRepository,
+        IDeviceInformationServiceMock deviceInformationService
+    )
+    {
+        _twoDHomeRepository = twoDHomeRepository;
+        _roomRepository = roomRepository;
+        _deviceInformationService = deviceInformationService;
+    }
+
+    public ITwoDHomeWebResponse GetAllRoomGridsRelatedToAccount(Guid accountId)
+    {
+        var allRoomCoordinatesList = _twoDHomeRepository.GetAllRoomCoordinatesRelatedToAccount(accountId);
+        if (!allRoomCoordinatesList.Any())
+            return TwoDHomeWebResponseFactory.CreateRoomWebResponse(new List<RoomGrid>());
+
+        var allRooms = _roomRepository.GetRoomsRelatedToAccount(accountId).ToList();
+        var allRoomGrids = new List<RoomGrid>();
+        foreach (var room in allRooms)
+        {
+            // this handles the case where the room exists but do not have a room coordinate
+            if (room.RoomCoordinate == null) continue;
+            var devicesInRoom = _deviceInformationService.GetDevicesInRoom(room.RoomId).ToList();
+            room.Devices = devicesInRoom;
+            var deviceStateMapping = new Dictionary<Guid, bool>();
+            foreach (var device in room.Devices)
+                deviceStateMapping.Add(device.DeviceId, _deviceInformationService.isDeviceOn(device.DeviceId));
+            allRoomGrids.Add(RoomGridFactory.CreateRoomGrid(room, deviceStateMapping));
+        }
+
+        return TwoDHomeWebResponseFactory.CreateRoomWebResponse(allRoomGrids);
+    }
+
+    public async Task<IRoomCoordinateResponse?> GetRoomCoordinate(Guid roomCoordinateId)
+    {
+        var res = await _twoDHomeRepository.GetRoomCoordinateById(roomCoordinateId);
+        if (res == null) return null;
+        return RoomCoordinateResponseFactory.CreateRoomCoordinateResponse(res);
+    }
+
+    public async Task<IDeviceCoordinateResponse?> GetDeviceCoordinate(Guid deviceCoordinateId)
+    {
+        var res = await _twoDHomeRepository.GetDeviceCoordinateById(deviceCoordinateId);
+        if (res == null) return null;
+        return DeviceCoordinateResponseFactory.CreateDeviceCoordinateResponse(res);
+    }
+
+    // for testing purposes only
+    public async Task<IEnumerable<IDeviceCoordinate>> GetAllDeviceCoordinates()
+    {
+        var result = await _twoDHomeRepository.GetAllDeviceCoordinates();
+        return result;
+    }
+
+    public async Task<IEnumerable<IRoomCoordinate>> GetAllRoomCoordinates()
+    {
+        var result = await _twoDHomeRepository.GetAllRoomCoordinates();
+        return result;
+    }
+}
---
+++ SmartHomeManager/SmartHomeManager.Domain/TwoDHomeDomain/Services/TwoDHomeWriteService.cs
@@ -0,0 +1,78 @@
+using SmartHomeManager.Domain.RoomDomain.Interfaces;
+using SmartHomeManager.Domain.RoomDomain.Mocks;
+using SmartHomeManager.Domain.TwoDHomeDomain.DTOs.Responses;
+using SmartHomeManager.Domain.TwoDHomeDomain.Entities;
+using SmartHomeManager.Domain.TwoDHomeDomain.Factories;
+using SmartHomeManager.Domain.TwoDHomeDomain.Interfaces;
+using SmartHomeManager.Domain.TwoDHomeDomain.Mocks;
+
+namespace SmartHomeManager.Domain.TwoDHomeDomain.Services;
+
+public class TwoDHomeWriteService : ITwoDHomeWriteService
+{
+    private readonly IDeviceInformationServiceMock _deviceInformationService;
+    private readonly IRoomRepository _roomRepository;
+    private readonly ITwoDHomeRepository _twoDHomeRepository;
+    private readonly IControlDeviceServiceMock _updateDeviceService;
+
+    public TwoDHomeWriteService(ITwoDHomeRepository twoDHomeRepository, IControlDeviceServiceMock updateDeviceService,
+        IRoomRepository roomRepository, IDeviceInformationServiceMock deviceInformationService)
+    {
+        _twoDHomeRepository = twoDHomeRepository;
+        _updateDeviceService = updateDeviceService;
+        _roomRepository = roomRepository;
+        _deviceInformationService = deviceInformationService;
+    }
+
+    public ITwoDHomeWebResponse UpdateRoomGrids(Guid accountId, List<RoomGrid> roomGrids)
+    {
+        var allRoomCoordinates = _twoDHomeRepository.GetAllRoomCoordinatesRelatedToAccount(accountId);
+        _twoDHomeRepository.RemoveRange(allRoomCoordinates);
+
+        var updatedRoomCoordinates = new List<IRoomCoordinate>();
+        foreach (var room in roomGrids)
+            updatedRoomCoordinates.Add(RoomCoordinateFactory.CreateRoomCoordinate
+                (
+                    room.RoomCoordinateId,
+                    room.x,
+                    room.y,
+                    room.w,
+                    room.h,
+                    room.RoomId
+                )
+            );
+        _twoDHomeRepository.AddRange(updatedRoomCoordinates);
+        return TwoDHomeWebResponseFactory.CreateRoomWebResponse(roomGrids);
+    }
+
+    public bool ChangeDeviceState(Guid deviceId, bool state)
+    {
+        if (state)
+            _updateDeviceService.SwitchOnDevice(deviceId);
+        else
+            _updateDeviceService.SwitchOffDevice(deviceId);
+        return true;
+    }
+
+    public async Task<IRoomCoordinateResponse?> AddRoomCoordinate(int x, int y, int w, int h, Guid roomId)
+    {
+        var room = await _roomRepository.Get(roomId);
+        if (room == null) return null;
+        var roomCoordinate = _twoDHomeRepository.GetRoomCoordinateByRoomId(roomId);
+        if (roomCoordinate != null) return null;
+        var newRoomCoordinate = RoomCoordinateFactory.CreateRoomCoordinate(new Guid(), x, y, w, h, roomId);
+        await _twoDHomeRepository.AddRoomCoordinate(newRoomCoordinate);
+        return RoomCoordinateResponseFactory.CreateRoomCoordinateResponse(newRoomCoordinate);
+    }
+
+    public async Task<IDeviceCoordinateResponse?> AddDeviceCoordinate(int x, int y, int w, int h, Guid deviceId)
+    {
+        var device = _deviceInformationService.GetDeviceById(deviceId);
+        if (device == null) return null;
+        var deviceCoordinate = _twoDHomeRepository.GetDeviceCoordinateByDeviceId(deviceId);
+        if (deviceCoordinate != null) return null;
+        var newDeviceCoordinate = DeviceCoordinateFactory.CreateDeviceCoordinate(new Guid(), x, y, w, h, deviceId);
+        await _twoDHomeRepository.AddDeviceCoordinate(newDeviceCoordinate);
+        return DeviceCoordinateResponseFactory.CreateDeviceCoordinateResponse(newDeviceCoordinate);
+    }
+}
