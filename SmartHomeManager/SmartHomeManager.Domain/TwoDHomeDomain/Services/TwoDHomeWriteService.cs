1e320178ed4d2647f1e6efbf9d44da55a771cbf0
Pang Ka Ho <71180280+pangkaho14@users.noreply.github.com>
2023-03-15 01:50:57 +0800

    code: added documentation for validations in TwoDHomeWriteService.cs

--- SmartHomeManager/SmartHomeManager.Domain/TwoDHomeDomain/Services/TwoDHomeWriteService.cs
+++ SmartHomeManager/SmartHomeManager.Domain/TwoDHomeDomain/Services/TwoDHomeWriteService.cs
@@ -56,8 +56,11 @@ public class TwoDHomeWriteService : ITwoDHomeWriteService
 
     public async Task<IRoomCoordinateResponse?> AddRoomCoordinate(int x, int y, int w, int h, Guid roomId)
     {
+        // validation: roomId must refer to an existing room in order to create a roomCoordinate
         var room = await _roomRepository.Get(roomId);
         if (room == null) return null;
+
+        // validation: roomCoordinate must not exist in order to create a new one 
         var roomCoordinate = _twoDHomeRepository.GetRoomCoordinateByRoomId(roomId);
         if (roomCoordinate != null) return null;
         var newRoomCoordinate = RoomCoordinateFactory.CreateRoomCoordinate(new Guid(), x, y, w, h, roomId);
@@ -67,8 +70,11 @@ public class TwoDHomeWriteService : ITwoDHomeWriteService
 
     public async Task<IDeviceCoordinateResponse?> AddDeviceCoordinate(int x, int y, int w, int h, Guid deviceId)
     {
+        // validation: deviceId must refer to an existing device in order to create a deviceCoordinate
         var device = _deviceInformationService.GetDeviceById(deviceId);
         if (device == null) return null;
+
+        // validation: deviceCoordinate must not exist in order to create a new one
         var deviceCoordinate = _twoDHomeRepository.GetDeviceCoordinateByDeviceId(deviceId);
         if (deviceCoordinate != null) return null;
         var newDeviceCoordinate = DeviceCoordinateFactory.CreateDeviceCoordinate(new Guid(), x, y, w, h, deviceId);
