eb70bd4bf08a31efd92b3370bda59ea2f9c9bbcf
Pang Ka Ho <71180280+pangkaho14@users.noreply.github.com>
2023-03-15 00:24:01 +0800

    code: added documentation for why RoomGrid concrete type is used

--- SmartHomeManager/SmartHomeManager.Domain/TwoDHomeDomain/DTOs/Responses/ITwoDHomeWebResponse.cs
+++ SmartHomeManager/SmartHomeManager.Domain/TwoDHomeDomain/DTOs/Responses/ITwoDHomeWebResponse.cs
@@ -2,10 +2,13 @@
 
 namespace SmartHomeManager.Domain.TwoDHomeDomain.DTOs.Responses;
 
-// using concrete classes for RoomGrid because ASP.Net does not support serialization of interface types,
-// thus a concrete class is used instead.
-// in addition, this is unlikely to change unless the client requirements change,
+// Using concrete classes for RoomGrid because ASP.Net does not support serialization of interface types,
+// thus a concrete class is used instead. This is because the RoomGrid is used as a request object in the controllers,
+// which ASP.net uses for serialization. See TwoDHomeWebRequest.cs.
+// In addition, this is unlikely to change unless the client requirements change,
 // thus we decided to use the concrete class instead
+
+// likewise for DeviceControl, because it is used inside RoomGrid, it has to be concrete as well
 public interface ITwoDHomeWebResponse
 {
     public List<RoomGrid> RoomGrids { get; set; }
