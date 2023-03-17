295e7e7807be40427af8d9590c32d1cac28a05ff
p1816246 <khorchai@hotmail.co.uk>
2023-03-18 04:04:39 +0800

    update config page ui

--- SmartHomeManager/SmartHomeManager.View/src/components/Devices/ManageDeviceConfigurationCard.js
+++ SmartHomeManager/SmartHomeManager.View/src/components/Devices/ManageDeviceConfigurationCard.js
@@ -1,5 +1,5 @@
+import { FormControl, FormLabel, Select } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { Heading, Card, CardHeader, CardBody, Text, CardFooter, Button, Select } from "@chakra-ui/react";
 
 export default function ManageDeviceConfigurationCard(props) {
     const possibleConfigurations = props.possibleConfigurations
@@ -10,35 +10,58 @@ export default function ManageDeviceConfigurationCard(props) {
         }));
 
     return (
-        <Card>
-            <CardHeader>
-                <Heading size="md">{`Configure ${possibleConfigurations.name}`}</Heading>
-            </CardHeader>
+        <FormControl>
+            <FormLabel>Configure {possibleConfigurations.name}</FormLabel>
+            <Select onChange={(e) => {
+                const selectedIndex = possibleConfigurations.values.findIndex((value) => value === e.target.value);
+                props.handleDeviceConfiguration({
+                    configurationKey: possibleConfigurations.name,
+                    configurationValue: selectedIndex
+                })
+            }} defaultValue={actualConfigurationKey[0] ? actualConfigurationKey[0].configurationValue : 0}>
+                {possibleConfigurations.values.length > 0 ? (
+                    possibleConfigurations.values.map((configuration, i) => (
+                        <option
+                            key={i}
+                            value={configuration}
+                        >
+                            {configuration}
+                        </option>
+                    ))
+                ) : (
+                    <p>None available.</p>
+                )}
+            </Select>
+        </FormControl>
+        // <Card>
+        //     <CardHeader>
+        //         <Heading size="md">{`Configure ${possibleConfigurations.name}`}</Heading>
+        //     </CardHeader>
 
-            <CardBody>
-                <Select onChange={(e) => {
-                    const selectedIndex = possibleConfigurations.values.findIndex((value) => value === e.target.value);
-                    props.handleDeviceConfiguration({
-                        configurationKey: possibleConfigurations.name,
-                        configurationValue: selectedIndex
-                    })
-                }} defaultValue={actualConfigurationKey[0] ? actualConfigurationKey[0].configurationValue : 0}>
-                    {possibleConfigurations.values.length > 0 ? (
-                        possibleConfigurations.values.map((configuration, i) => (
-                            <option
-                                key={i}
-                                value={configuration}
-                            >
-                                {configuration}
-                            </option>
-                        ))
-                    ) : (
-                        <p>None available.</p>
-                    )}
-                </Select>
-            </CardBody>
-            <CardFooter flex="row" justifyContent="flex-end">
-            </CardFooter>
-        </Card>
+        //     <CardBody>
+        //         <Select onChange={(e) => {
+        //             const selectedIndex = possibleConfigurations.values.findIndex((value) => value === e.target.value);
+        //             props.handleDeviceConfiguration({
+        //                 configurationKey: possibleConfigurations.name,
+        //                 configurationValue: selectedIndex
+        //             })
+        //         }} defaultValue={actualConfigurationKey[0] ? actualConfigurationKey[0].configurationValue : 0}>
+        //             {possibleConfigurations.values.length > 0 ? (
+        //                 possibleConfigurations.values.map((configuration, i) => (
+        //                     <option
+        //                         key={i}
+        //                         value={configuration}
+        //                     >
+        //                         {configuration}
+        //                     </option>
+        //                 ))
+        //             ) : (
+        //                 <p>None available.</p>
+        //             )}
+        //         </Select>
+        //     </CardBody>
+        //     <CardFooter flex="row" justifyContent="flex-end">
+        //     </CardFooter>
+        // </Card>
     );
 }
--- SmartHomeManager/SmartHomeManager.View/src/pages/device/manage-device/ManageDeviceConfiguration.js
+++ SmartHomeManager/SmartHomeManager.View/src/pages/device/manage-device/ManageDeviceConfiguration.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Text, Tabs, TabList, Tab, TabPanels, TabPanel, Button, Stack, useToast, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, FormControl, FormLabel, Input, FormHelperText } from "@chakra-ui/react";
+import { Container, Text, Tabs, TabList, Tab, TabPanels, TabPanel, Button, Stack, useToast, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, FormControl, FormLabel, Input, FormHelperText, Flex, Heading, Box } from "@chakra-ui/react";
 import { useSearchParams } from "react-router-dom";
 import ManageDeviceConfigurationCard from "../../../components/Devices/ManageDeviceConfigurationCard";
 
@@ -12,7 +12,7 @@ export default function ManageDeviceConfiguration() {
   const [devicePossibleConfigurations, setDevicePossibleConfigurations] = useState([]);
   const [deviceActualConfigurations, setDeviceActualConfigurations] = useState([]);
 
-      // for import device
+  // for import device
   const [showImportDeviceModal, setShowImportDeviceModal] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
 
@@ -29,73 +29,73 @@ export default function ManageDeviceConfiguration() {
 
   function resetImportHooks() {
     setShowImportDeviceModal(false);
-    
-    }
+
+  }
 
   useEffect(() => {
-      fetchDeviceConfigurations()
+    fetchDeviceConfigurations()
   }, []);
 
-    function fetchDeviceConfigurations() {
-        fetch(`https://localhost:7140/api/ManageDevice/GetDevicePossibleConfigurations/${deviceBrand}/${deviceModel}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setDevicePossibleConfigurations(data);
-            });
-        fetch(`https://localhost:7140/api/ManageDevice/GetDeviceConfigurations/${deviceId}/${deviceBrand}/${deviceModel}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setDeviceActualConfigurations(data);
-            });
-    }
+  function fetchDeviceConfigurations() {
+    fetch(`https://localhost:7140/api/ManageDevice/GetDevicePossibleConfigurations/${deviceBrand}/${deviceModel}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setDevicePossibleConfigurations(data);
+      });
+    fetch(`https://localhost:7140/api/ManageDevice/GetDeviceConfigurations/${deviceId}/${deviceBrand}/${deviceModel}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setDeviceActualConfigurations(data);
+      });
+  }
 
 
-    function handleDeviceConfiguration({ configurationKey, configurationValue }) {
-        fetch("https://localhost:7140/api/ManageDevice/ApplyDeviceConfiguration", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                configurationKey,
-                deviceBrand,
-                deviceModel,
-                deviceId,
-                configurationValue,
-            }),
-        })
-            .then((response) => {
-                if (response.ok) {
-                    toast({
-                        title: "Success",
-                        description: "Device Configuration has been added successfully.",
-                        status: "success",
-                        duration: 9000,
-                        isClosable: true,
-                    });
-                } else {
-                    toast({
-                        title: "Error",
-                        description: "Device Configuration adding failed.",
-                        status: "error",
-                        duration: 9000,
-                        isClosable: true,
-                    });
-                }
-                fetchDeviceConfigurations();
-                onClose();
-            });
-    }
+  function handleDeviceConfiguration({ configurationKey, configurationValue }) {
+    fetch("https://localhost:7140/api/ManageDevice/ApplyDeviceConfiguration", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        configurationKey,
+        deviceBrand,
+        deviceModel,
+        deviceId,
+        configurationValue,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          toast({
+            title: "Success",
+            description: "Device Configuration has been added successfully.",
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+          });
+        } else {
+          toast({
+            title: "Error",
+            description: "Device Configuration adding failed.",
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+          });
+        }
+        fetchDeviceConfigurations();
+        onClose();
+      });
+  }
 
-    function handleExportDevice(e) {
-      e.preventDefault();
-      fetch(`https://localhost:7140/api/ManageDevice/ExportDeviceConfigurations/${deviceId}/${deviceBrand}/${deviceModel}`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          deviceId: deviceId,
-          deviceBrand: deviceBrand,
-          deviceModel: deviceModel,
-        }),
-      })
+  function handleExportDevice(e) {
+    e.preventDefault();
+    fetch(`https://localhost:7140/api/ManageDevice/ExportDeviceConfigurations/${deviceId}/${deviceBrand}/${deviceModel}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        deviceId: deviceId,
+        deviceBrand: deviceBrand,
+        deviceModel: deviceModel,
+      }),
+    })
       .then((response) => {
         if (response.ok) {
           toast({
@@ -122,69 +122,69 @@ export default function ManageDeviceConfiguration() {
       .catch((error) => {
         console.error('Error exporting device:', error);
       });
-    }
-    
-    function downloadJson(data, fileName) {
-      const jsonData = JSON.stringify(data);
-      const blob = new Blob([jsonData], { type: "application/json" });
-      const url = URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = fileName;
-      link.click();
-    }
+  }
 
-function setImportDevice(e) {
-  e.preventDefault();
-  setShowImportDeviceModal(true);
-  onOpen();
-}
+  function downloadJson(data, fileName) {
+    const jsonData = JSON.stringify(data);
+    const blob = new Blob([jsonData], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = fileName;
+    link.click();
+  }
 
-    
-function handleImportDevice(e) {
-  e.preventDefault();
-  const reader = new FileReader();
-  reader.onload = (event) => {
-    const deviceConfigurationJson = event.target.result;
-    fetch(`https://localhost:7140/api/ManageDevice/ImportDeviceConfigurations/${deviceId}/${deviceConfigurationJson}`, {
-      method: "POST",
-      body: JSON.stringify({
-        deviceBrand: deviceBrand,
-        deviceModel: deviceModel,
-        deviceConfigurationJson: deviceConfigurationJson,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          toast({
-            title: "Success",
-            description: "Device has been imported successfully.",
-            status: "success",
-            duration: 9000,
-            isClosable: true,
-          });
-          return response.json(); // parse the response and return data
-        } else {
-          toast({
-            title: "Error",
-            description: "Device import failed.",
-            status: "error",
-            duration: 9000,
-            isClosable: true,
-          });
-        }
+  function setImportDevice(e) {
+    e.preventDefault();
+    setShowImportDeviceModal(true);
+    onOpen();
+  }
+
+
+  function handleImportDevice(e) {
+    e.preventDefault();
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const deviceConfigurationJson = event.target.result;
+      fetch(`https://localhost:7140/api/ManageDevice/ImportDeviceConfigurations/${deviceId}/${deviceConfigurationJson}`, {
+        method: "POST",
+        body: JSON.stringify({
+          deviceBrand: deviceBrand,
+          deviceModel: deviceModel,
+          deviceConfigurationJson: deviceConfigurationJson,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .finally(() => {
-        setSelectedFile(null);
-        handleCloseModal();
-        fetchDeviceConfigurations();
-      });
-  };
-  reader.readAsText(selectedFile);
-}
+        .then((response) => {
+          if (response.ok) {
+            toast({
+              title: "Success",
+              description: "Device has been imported successfully.",
+              status: "success",
+              duration: 9000,
+              isClosable: true,
+            });
+            return response.json(); // parse the response and return data
+          } else {
+            toast({
+              title: "Error",
+              description: "Device import failed.",
+              status: "error",
+              duration: 9000,
+              isClosable: true,
+            });
+          }
+        })
+        .finally(() => {
+          setSelectedFile(null);
+          handleCloseModal();
+          fetchDeviceConfigurations();
+        });
+    };
+    reader.readAsText(selectedFile);
+  }
 
 
 
@@ -192,56 +192,101 @@ function handleImportDevice(e) {
     return <Text> Loading </Text>;
   } else
     return (
-      <Container mt={5} mb={5} p={5} maxW="3xl" minH="50vh" border="1px" borderColor="gray.100" rounded="lg" boxShadow="lg" centerContent>
-        <Text fontWeight="bold" fontSize="xl" mb={5}>
-          {`Manage ${deviceName} configurations`}
-        </Text>
+      <Container mt={5}>
+        <Flex
+          align={'center'}
+          justify={'center'}>
+          <Stack spacing={8} mx={'auto'} minW={'xl'} py={12} px={6}>
+            <Stack align={'center'}>
+              <Heading fontSize={'4xl'}>{deviceName} Configurations</Heading>
+            </Stack>
+            <Box
+              rounded={'lg'}
+              bg='white'
+              boxShadow={'lg'}
+              p={8}>
+              <Stack spacing={4}>
+                {devicePossibleConfigurations.length > 0 ? (
+                  devicePossibleConfigurations.map((configuration, i) => (
+                    <ManageDeviceConfigurationCard
+                      key={i}
+                      actualConfigurations={deviceActualConfigurations}
+                      possibleConfigurations={configuration}
+                      handleDeviceConfiguration={(deviceConfiguration) => handleDeviceConfiguration(deviceConfiguration)}
+                    />
+                  ))
+                ) : (
+                  <p>None available.</p>
+                )}
+                <Stack pt={5} spacing={6} direction={['column', 'row']} justifyContent="space-between">
+                  <Button
+                    bg={'blue.400'}
+                    color={'white'}
+                    _hover={{
+                      bg: 'blue.500',
+                    }}
+                    onClick={setImportDevice}>
+                    Import Configurations
+                  </Button>
+                  <Button
+                    bg={'green.400'}
+                    color={'white'}
+                    _hover={{
+                      bg: 'green.500',
+                    }}
+                    onClick={handleExportDevice}>
+                    Export Configurations
+                  </Button>
+                </Stack>
+              </Stack>
+            </Box>
+          </Stack>
+        </Flex>
 
-        
-    {showImportDeviceModal &&
-      <Modal isOpen={isOpen} onClose={handleCloseModal}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Upload your Device Configuration File</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <form
-              id="import-device-form"
-              onSubmit={(e) => handleImportDevice(e)}
-            >
-              <FormControl isRequired>
-                <FormLabel> File </FormLabel>
-                <Input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} sx={{ border: 'none', boxShadow: 'none', '&:focus': { boxShadow: 'none' } }}/>
-                <FormHelperText>
-                  Ensure your file is in the proper format
-                </FormHelperText>
-              </FormControl>
-            </form>
-          </ModalBody>
-          <ModalFooter>
-            <Button type="submit" form="import-device-form" colorScheme='green' mr={3}>
-              Import Device Configuration
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    }
-        <Stack spacing={5}>
+        {showImportDeviceModal &&
+          <Modal isOpen={isOpen} onClose={handleCloseModal}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Upload your Device Configuration File</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <form
+                  id="import-device-form"
+                  onSubmit={(e) => handleImportDevice(e)}
+                >
+                  <FormControl isRequired>
+                    <FormLabel> File </FormLabel>
+                    <Input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} sx={{ border: 'none', boxShadow: 'none', '&:focus': { boxShadow: 'none' } }} />
+                    <FormHelperText>
+                      Ensure your file is in the proper format
+                    </FormHelperText>
+                  </FormControl>
+                </form>
+              </ModalBody>
+              <ModalFooter>
+                <Button type="submit" form="import-device-form" colorScheme='green' mr={3}>
+                  Import Device Configuration
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        }
+        {/* <Stack spacing={5}>
           {devicePossibleConfigurations.length > 0 ? (
             devicePossibleConfigurations.map((configuration, i) => (
               <ManageDeviceConfigurationCard
-                    key={i}
-                    actualConfigurations={deviceActualConfigurations}
-                    possibleConfigurations={configuration}
-                    handleDeviceConfiguration={(deviceConfiguration) => handleDeviceConfiguration(deviceConfiguration)}
+                key={i}
+                actualConfigurations={deviceActualConfigurations}
+                possibleConfigurations={configuration}
+                handleDeviceConfiguration={(deviceConfiguration) => handleDeviceConfiguration(deviceConfiguration)}
               />
             ))
           ) : (
             <p>None available.</p>
           )}
-              <Button mr={3} onClick={setImportDevice} colorScheme="red">Import Device Configurations</Button>
-              <Button onClick={handleExportDevice} colorScheme="yellow">Export Device Configurations</Button>
-        </Stack>
+          <Button mr={3} onClick={setImportDevice} colorScheme="red">Import Device Configurations</Button>
+          <Button onClick={handleExportDevice} colorScheme="yellow">Export Device Configurations</Button>
+        </Stack> */}
       </Container>
     );
 }
