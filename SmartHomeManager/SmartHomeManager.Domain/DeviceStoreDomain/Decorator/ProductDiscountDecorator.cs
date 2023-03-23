8b5a4144ddc80853c4cfd4a69e20ed10395bb00e
Pang Ka Ho <71180280+pangkaho14@users.noreply.github.com>
2023-03-23 18:54:15 +0800

    merge conflict

--- SmartHomeManager/SmartHomeManager.Domain/DeviceStoreDomain/Decorator/ProductDiscountDecorator.cs
+++ SmartHomeManager/SmartHomeManager.Domain/DeviceStoreDomain/Decorator/ProductDiscountDecorator.cs
@@ -1,17 +1,18 @@
-using SmartHomeManager.Domain.DeviceStoreDomain.Interfaces;
 using SmartHomeManager.Domain.DeviceStoreDomain.Entities;
 
 namespace SmartHomeManager.Domain.DeviceStoreDomain.Decorator;
-public abstract class ProductDiscountDecorator : IDeviceProducts
+
+public abstract class ProductDiscountDecorator : IDeviceProduct
 {
-    protected readonly IDeviceProducts _product;
+    protected readonly IDeviceProduct _product;
     protected double? _discountedPrice;
 
-    protected ProductDiscountDecorator(IDeviceProducts product)
+    protected ProductDiscountDecorator(IDeviceProduct product)
     {
         _product = product;
         ApplyDiscount();
     }
+
     public DeviceProduct UndecoratedProduct => _product as DeviceProduct; // Add this property
 
     public int ProductId => _product.ProductId;
@@ -21,13 +22,14 @@ public abstract class ProductDiscountDecorator : IDeviceProducts
     public string DeviceType => _product.DeviceType;
     public string ProductDescription => _product.ProductDescription;
     public double ProductPrice => _discountedPrice ?? _product.ProductPrice;
+
     public int ProductQuantity
     {
         get => _product.ProductQuantity;
         set => _product.ProductQuantity = value; // Implement the setter here
     }
+
     public string ProductImageUrl => _product.ProductImageUrl;
 
     protected abstract void ApplyDiscount();
-
 }
