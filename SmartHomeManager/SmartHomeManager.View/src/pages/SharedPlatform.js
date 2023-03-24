45b25029d1c7c9e4eadf161512a142af2ce7ba4b
Lavenzaa <94179890+Lavenzaa@users.noreply.github.com>
2023-03-24 22:51:53 +0800

    Front End now displays Profiles, Scenarios and Rules Dynamically, also added functionality of being able to copy scenario

--- SmartHomeManager/SmartHomeManager.View/src/pages/SharedPlatform.js
+++ SmartHomeManager/SmartHomeManager.View/src/pages/SharedPlatform.js
@@ -1,92 +1,94 @@
 import React, { useEffect, useState } from "react";
 import { SharedProfileCardComponent } from "pages/profile/SharedCard";
-import { Grid, GridItem, Box } from "@chakra-ui/react";
+import { Grid, Box } from "@chakra-ui/react";
 import user1 from "./profile/img/user1.png";
 
 export default function SharedPlatform() {
-    const [scenarios, updateScenarios] = useState([]);
-    useEffect(() => {
-        getScenarios()
-        console.log(scenarios);
-    },[])
-  const getScenarios = async () => {
-    const profileId = "22222222-2222-2222-2222-222222222222";
-    await fetch(
-      `https://localhost:7140/api/Scenarios/profileId?profileId=${profileId}`,
-      {
-        method: "GET",
-        headers: {
-          accept: "text/plain",
-        },
-      }
-    ).then(async (response) => {
-      const data = await response.json();
+  const [scenarios, updateScenarios] = useState([]);
+  const [scenarioData, setScenarioData] = useState({});
 
+  useEffect(() => {
+    const getScenarios = async () => {
+      const profileId = "22222222-2222-2222-2222-222222222222";
+      const response = await fetch(
+        `https://localhost:7140/api/Scenarios/profileId?profileId=${profileId}`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+          },
+        }
+      );
+      const data = await response.json();
       if (response.ok) {
         updateScenarios(data);
-        
       }
-    });
     };
-  //TODO: Call Controller
-  return (
-    <>
-      <Grid
-        templateRows="repeat(3, 1fr)"
-        gap={3}
-        paddingTop="3em"
-        paddingRight="3em"
-          >
+    getScenarios();
+  }, []);
 
-              {/* NEED DO HAVENT DO PROPERLY, MAP ITEMS IN SCENARIO 
-              {
-                  scenarios.map((item) => (
-                      <Stack key={item.isshareable} align={'center'}>
-                          <div>
-                              Scan the QR code with your mobile device!
-                          </div>
-                          <Image
-                              src={item.authenticationBarCodeImage}
-                              boxSize='250px'
-                          >
-                          </Image>
-                          <div>
-                              Your Manual Setup Key:
-                          </div>
-                          <div>
-                              {item.authenticationManualCode}
-                          </div>
-                      </Stack>
-                  ))
-              }*/}
-        <Box w="100%" m="0 auto" h="-moz-initial" bg="gray.50">
-          <SharedProfileCardComponent
-            ProfileName="Juleus"
-            Temp="20.5"
-            Light="Bright"
-            TV="Off"
-            ProfilePic={user1}
-          />
-        </Box>
-        <Box w="100%" m="0 auto" h="moz-initial" bg="gray.50">
-          <SharedProfileCardComponent
-            ProfileName="KC"
-            Temp="25"
-            Light="Dim"
-            TV="On"
-            ProfilePic={user1}
-          />
-        </Box>
-        <Box w="100%" m="0 auto" h="-moz-initial" bg="gray.50">
-          <SharedProfileCardComponent
-            ProfileName="Ryan"
-            Temp="18 "
-            Light="Ultra Bright"
-            TV="On"
-            ProfilePic={user1}
-          />
-        </Box>
-      </Grid>
-    </>
+  useEffect(() => {
+    const fetchData = async (scenarioId) => {
+      const response = await fetch(
+        `https://localhost:7140/api/RulesControllerMock/GetByScenariosId/${scenarioId}`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      if (response.ok) {
+        setScenarioData((prevData) => ({
+          ...prevData,
+          [scenarioId]: data,
+        }));
+      }
+    };
+
+    scenarios.forEach((scenario) => {
+      fetchData(scenario.scenarioId);
+    });
+  }, [scenarios]);
+
+  return (
+    <Grid
+      templateRows="repeat(3, 1fr)"
+      gap={3}
+      paddingTop="3em"
+      paddingRight="3em"
+    >
+      {scenarios.map((item) => {
+        if (item.isShareable) {
+          return (
+            //Only display those profiles which are shareable to the community
+            <Box key={item.profileId} w="100%" m="0 auto" bg="gray.50">
+              <SharedProfileCardComponent
+                ProfileName={item.profileId}
+                SName={item.scenarioName}
+                SId={item.scenarioId}
+                IA={item.IsActive}
+                ProfilePic={user1}
+                Rules={scenarioData[item.scenarioId]?.map((item) => (
+                  <div key={item.ruleName}>
+                    {"\n\nRule Name: " + item.ruleName}
+                    <br />
+                    {"\nConfig Key: " + item.configurationKey}
+                    <br />
+                    {"\nConfiguration Value: " + item.configurationValue}
+                    <br />
+                    {"\nRule Id: " + item.ruleId}
+                    <br />
+                    <br />
+                  </div>
+                ))}
+                // Use optional chaining to prevent errors
+              />
+            </Box>
+          );
+        }
+      })}
+    </Grid>
   );
 }
--- SmartHomeManager/SmartHomeManager.View/src/pages/profile/SharedCard.js
+++ SmartHomeManager/SmartHomeManager.View/src/pages/profile/SharedCard.js
@@ -1,88 +1,102 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Stack,
-    Heading,
-    Text,
-    Button,
-    Image,
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    PopoverHeader,
-    PopoverBody,
-    PopoverFooter,
-    PopoverArrow,
-    PopoverCloseButton,
-    PopoverAnchor,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
 } from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
 import { css } from "@emotion/react";
 
+function SharedProfileCard({ ProfileName, SName, SId, ProfilePic, Rules, IA }) {
+  const textAreaRef = useRef(null);
 
-function SharedProfileCard({ ProfileName, Temp, Light, TV, ProfilePic }) {
-    return (
-        <Card
-            direction={{ base: "column", sm: "row" }}
-            overflow="hidden"
-            variant="outline"
-        >
-            <Image
-                objectFit="contain"
-                maxW={{ base: "100%", sm: "200px" }}
-                src={ProfilePic}
-                alt="Profile Pic"
-            />
+  const handleCopyClick = () => {
+    if (textAreaRef.current) {
+      const text = textAreaRef.current.textContent;
+      navigator.clipboard.writeText(text).then(() => {
+        console.log("Copied to clipboard:", text);
+      });
+    }
+  };
 
-            <Stack>
-                <CardBody>
-                    <Heading size="md">{ProfileName} Profile</Heading>
+  return (
+    <Card direction={{ base: "column", sm: "row" }} variant="outline">
+      <Image
+        objectFit="contain"
+        maxW={{ base: "100%", sm: "200px" }}
+        src={ProfilePic}
+        alt="Profile Pic"
+      />
 
-                    <Text>Temperature: {Temp}</Text>
-                    <Text>Light: {Light}</Text>
-                    <Text>TV: {TV}</Text>
-                </CardBody>
-                <CardFooter>
-                    <Popover>
-                        <PopoverTrigger>
-                            <Button variant="solid" colorScheme="green">
-                        More Details
-                    </Button>
-                        </PopoverTrigger>
-                        <PopoverContent color='white' bg='blue.700' borderColor='blue.800'>
-                            <PopoverArrow />
-                            <PopoverCloseButton />
-                            <PopoverHeader fontWeight="bold">Scenario Rules</PopoverHeader>
-                            <PopoverBody>{Temp}<br/> {Light}<br/> {TV}</PopoverBody>
-                        </PopoverContent>
-                    </Popover>
-                    
-                    <Button marginLeft="10px" variant="solid" colorScheme="blue">
-                        Use Profile
-                    </Button>
-                </CardFooter>
-            </Stack>
-        </Card>
-    );
+      <Stack>
+        <CardBody>
+          <Heading size="md">{ProfileName} Profile</Heading>
+
+          <Text>Scenario Name: {SName}</Text>
+          <Text>Scenario Id: {SId}</Text>
+          <Text>is Active: {IA}</Text>
+        </CardBody>
+        <CardFooter>
+          <Popover>
+            <PopoverTrigger>
+              <Button variant="solid" colorScheme="green">
+                More Details
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent color="white" bg="blue.700" borderColor="blue.800">
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverHeader fontWeight="bold">Scenario Rules</PopoverHeader>
+              <PopoverBody ref={textAreaRef}>{Rules}</PopoverBody>
+              <PopoverFooter>
+                <Button
+                  marginLeft="10px"
+                  variant="solid"
+                  colorScheme="blue"
+                  onClick={handleCopyClick}
+                >
+                  Copy to Clipboard
+                </Button>
+              </PopoverFooter>
+            </PopoverContent>
+          </Popover>
+        </CardFooter>
+      </Stack>
+    </Card>
+  );
 }
 
 export function SharedProfileCardComponent({
-    ProfileName,
-    Temp,
-    Light,
-    TV,
-    ProfilePic,
+  ProfileName,
+  SName,
+  SId,
+  ProfilePic,
+  Rules,
+  IA,
 }) {
-    return (
-        <SharedProfileCard
-            ProfileName={ProfileName}
-            Temp={Temp}
-            Light={Light}
-            TV={TV}
-            ProfilePic={ProfilePic}
-        />
-    );
+  return (
+    <SharedProfileCard
+      IA={IA}
+      Rules={Rules}
+      ProfileName={ProfileName}
+      SName={SName}
+      SId={SId}
+      ProfilePic={ProfilePic}
+    />
+  );
 }
