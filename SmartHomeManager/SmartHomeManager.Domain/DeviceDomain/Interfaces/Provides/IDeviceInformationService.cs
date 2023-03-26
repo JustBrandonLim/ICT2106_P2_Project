bca8151157c5fb4330527bb4ec927e502dae3f4a
Brandon Lim Jing Zhi <brandonlimjingzhi@gmail.com>
2023-03-26 14:51:28 +0800

    Added IDeviceInformationService.

---
+++ SmartHomeManager/SmartHomeManager.Domain/DeviceDomain/Interfaces/Provides/IDeviceInformationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SmartHomeManager.Domain.DeviceDomain.Entities;
+
+namespace SmartHomeManager.Domain.DeviceDomain.Interfaces.Provides
+{
+	public interface IDeviceInformationService
+	{
+		public Task<Device?> GetDeviceByIdAsync(Guid deviceId);
+
+		public Task<IEnumerable<Device>> GetDevicesInRoomAsync(Guid roomId);
+
+		public Task<bool> IsDeviceOn(Guid deviceId);
+	}
+}
+
---
+++ SmartHomeManager/SmartHomeManager.Domain/DeviceDomain/Services/Provides/DeviceInformationService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SmartHomeManager.Domain.DeviceDomain.Entities;
+using SmartHomeManager.Domain.DeviceDomain.Interfaces;
+using SmartHomeManager.Domain.DeviceDomain.Interfaces.Provides;
+
+namespace SmartHomeManager.Domain.DeviceDomain.Services.Provides
+{
+	public class DeviceInformationService : IDeviceInformationService
+	{
+        private readonly IDeviceRepository _deviceRepository;
+        private readonly IDeviceConfigurationRepository _deviceConfigurationRepository;
+
+        public DeviceInformationService(IDeviceRepository deviceRepository, IDeviceConfigurationRepository deviceConfigurationRepository) 
+        { 
+            _deviceRepository = deviceRepository;
+            _deviceConfigurationRepository = deviceConfigurationRepository;
+        }
+
+        public async Task<Device?> GetDeviceByIdAsync(Guid deviceId)
+        {
+            return await _deviceRepository.GetAsync(deviceId);
+        }
+
+        public async Task<IEnumerable<Device>> GetDevicesInRoomAsync(Guid roomId)
+        {
+            return (await _deviceRepository.GetAllAsync()).Where(device => device.RoomId == roomId);
+        }
+
+        public async Task<bool> IsDeviceOn(Guid deviceId)
+        {
+            Device? device = await _deviceRepository.GetAsync(deviceId);
+            if (device is null)
+                return false;
+
+            return (await _deviceConfigurationRepository.GetAsync("Power", deviceId))?.ConfigurationValue == 1;
+        }
+    }
+}
+
